1095733a50fd0a9670fedd91b58eee8616fcc9ca
Frank-Howd <frankl.howd@gmail.com>
2021-03-03 13:19:14 -0500

    Installed dash_daq.  Created daq gauges (speedometers) for predictions.py

--- pages/predictions.py
+++ pages/predictions.py
@@ -1,6 +1,7 @@
 # Imports from 3rd party libraries
 import dash
 import dash_bootstrap_components as dbc
+import dash_daq as daq
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
@@ -45,11 +46,25 @@ def update_T(input_value):
     return 'You\'ve entered {}%'.format(input_value)
 
 @app.callback(
+    dash.dependencies.Output('treas-daq-gauge', 'value'),
+    [dash.dependencies.Input('10-year-T', 'value')]
+)
+def update_output(value):
+    return value
+
+@app.callback(
     Output('print-cp', 'children'),
     [Input('Corporate-Profits', 'value')],
 )
 def update_cp(input_value):
     return f'You\'ve entered ${input_value:,.0f} Billion'
+
+@app.callback(
+    dash.dependencies.Output('corp-prof', 'value'),
+    [dash.dependencies.Input('Corporate-Profits', 'value')]
+)
+def update_output(value):
+    return value
 
 @app.callback(
     Output('print-gdp', 'children'),
@@ -59,6 +74,13 @@ def update_gdp(input_value):
     return 'You\'ve entered ${:,.0f} Billion'.format(input_value)
 
 @app.callback(
+    dash.dependencies.Output('gdp', 'value'),
+    [dash.dependencies.Input('GDP', 'value')]
+)
+def update_output(value):
+    return value
+
+@app.callback(
     Output('print-net-exp', 'children'),
     [Input('Net-Exports', 'value')],
 )
@@ -66,11 +88,25 @@ def update_net_exp(input_value):
     return 'You\'ve entered ${} Billion'.format(input_value)
 
 @app.callback(
+    dash.dependencies.Output('net-exp', 'value'),
+    [dash.dependencies.Input('Net-Exports', 'value')]
+)
+def update_output(value):
+    return value
+
+@app.callback(
     Output('print-unemp-rate', 'children'),
     [Input('Unemployment-Rate', 'value')],
 )
 def update_unemp(input_value):
     return 'You\'ve entered {}%'.format(input_value)
+
+@app.callback(
+    dash.dependencies.Output('umpl-rate', 'value'),
+    [dash.dependencies.Input('Unemployment-Rate', 'value')]
+)
+def update_output(value):
+    return value
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
@@ -98,6 +134,13 @@ column2 = dbc.Col(
     [
         #dcc.Markdown('## Predictions', className='mb-5'),
         dcc.Markdown('#### 10-year Treasury (%)'),
+        html.Div(id='print-10yr', className='mb-3'),
+        daq.Gauge(
+            id='treas-daq-gauge',
+            max=16.0,
+            value=1.4,
+            min=0.0
+        ),
         dcc.Slider(
             id='10-year-T',
             min=0.0,
@@ -107,22 +150,27 @@ column2 = dbc.Col(
             marks={n: str(n) for n in range(0,17,2)},
             className='mb-3'
         ),
-        html.Div(id='print-10yr', className='mb-3'),
         dcc.Markdown(
             """___"""
         ),
         #dcc.Markdown('## Predictions', className='mb-5'),
         dcc.Markdown('#### Corporate Profits (Billions)'),
+        html.Div(id='print-cp', className='mb-3'),
+        daq.Gauge(
+            id='corp-prof',
+            max=3500,
+            value=500,
+            min=0
+        ),
         dcc.Slider(
             id='Corporate-Profits',
-            min=000,
+            min=0,
             max=3500,
             step=1,
             value=500,
-            marks={n: str(n) for n in range(00,3501,500)},
+            marks={n: str(n) for n in range(0,3501,500)},
             className='mb-3'
         ), 
-        html.Div(id='print-cp', className='mb-3'),
         dcc.Markdown(
             """___"""
         ),
@@ -150,6 +198,13 @@ column2 = dbc.Col(
         # ), 
         #dcc.Markdown('## Predictions', className='mb-5'),
         dcc.Markdown('#### U.S. Gross Domestic Product (Billions)'),
+        html.Div(id='print-gdp', className='mb-3'),
+        daq.Gauge(
+            id='gdp',
+            max=25000,
+            value=12500,
+            min=5000
+        ),
         dcc.Slider(
             id='GDP',
             min=5000,
@@ -159,7 +214,6 @@ column2 = dbc.Col(
             marks={n: str(n) for n in range(0,25001,2500)},
             className='mb-3'
         ),
-        html.Div(id='print-gdp', className='mb-3'),
         dcc.Markdown(
             """___"""
         ),
@@ -198,6 +252,13 @@ column2 = dbc.Col(
         # ),
         #dcc.Markdown('## Predictions', className='mb-5'),
         dcc.Markdown('#### Net Exports (Billions)'),
+        html.Div(id='print-net-exp', className='mb-3'),
+        daq.Gauge(
+            id='net-exp',
+            max=500,
+            value=-1000,
+            min=-740
+        ),
         dcc.Slider(
             id='Net-Exports',
             min=-1000,
@@ -207,12 +268,18 @@ column2 = dbc.Col(
             marks={n: str(n) for n in range(-1000,1000,250)},
             className='mb-3'
         ),
-        html.Div(id='print-net-exp', className='mb-3'),
         dcc.Markdown(
             """___"""
         ),
         #dcc.Markdown('## Predictions', className='mb-5'),
         dcc.Markdown('#### Unemployment Rate (%)'),
+        html.Div(id='print-unemp-rate', className='mb-3'),
+        daq.Gauge(
+            id='umpl-rate',
+            max=16.0,
+            value=8.0,
+            min=0.0
+        ),
         dcc.Slider(
             id='Unemployment-Rate',
             min=0.0,
@@ -222,7 +289,6 @@ column2 = dbc.Col(
             marks={n: str(n) for n in range(0,17,2)},
             className='mb-3'
         ),
-        html.Div(id='print-unemp-rate', className='mb-3'),
         dcc.Markdown(
             """___"""
         )
